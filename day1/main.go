58ada347dedb7b725239574759d1688a89a97614
Falase Femi <femifalase228@gmail.com>
2025-12-02 01:56:49 +0100

    complete solution for day one

--- day1/main.go
+++ day1/main.go
@@ -9,6 +9,13 @@ import (
 )
 
 func main() {
+	part1 := solvePart1()
+	part2 := solvePart2()
+	fmt.Println("Part 1 Password:", part1)
+	fmt.Println("Part 2 Password:", part2)
+}
+
+func solvePart1() int {
 	s := 50
 	count := 0
 
@@ -42,4 +49,46 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Password:", count)
+	return count
+}
+
+func solvePart2() int {
+	pos := 50
+	count := 0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) < 2 {
+			continue
+		}
+		dir := line[0]
+		dist, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		step := 1
+		if dir == 'L' {
+			step = -1
+		}
+		for i := 0; i < dist; i++ {
+			pos += step
+			if pos < 0 {
+				pos = 99
+			} else if pos > 99 {
+				pos = 0
+			}
+			if pos == 0 {
+				count++
+			}
+		}
+	}
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+	return count
 }
